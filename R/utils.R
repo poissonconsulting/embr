0edc363a9d82899062cbb301ee7c9f03173b90b0
Joe Thorley <joe@poissonconsulting.ca>
2016-10-11 09:00:30 -0700

    export dims

--- R/utils.R
+++ R/utils.R
@@ -1,1 +1,12 @@
-dims <- function(x) if (is.vector(x)) length(x) else dim(x)
+#' Dims
+#'
+#' @param x A vector, matrix or array.
+#' @return An integer vector of the dimensions.
+#' @examples
+#' dims(character())
+#' dims(2:3)
+#' dims(matrix(1:6, nrow = 2))
+#' @export
+dims <- function(x) {
+  if (is.vector(x)) length(x) else dim(x)
+}
