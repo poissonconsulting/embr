e4fdac6db05e75ad11ad2417c44f05607c0aba6b
Joe Thorley <joe@poissonconsulting.ca>
2016-11-03 07:24:06 -0700

    Add S3 function drop_parameters

---
+++ R/drop-parameters.R
@@ -0,0 +1,9 @@
+#' Drop Parameters
+#'
+#' Drops named scalar fixed parameters from a model by fixing them at 0.
+#'
+#' @param x The object.
+#' @param parameters A character vector of the parameters to drop.
+#' @return The updated object.
+#' @export
+drop_parameters <- function(x, parameters = character(0)) {UseMethod("drop_parameters")}
