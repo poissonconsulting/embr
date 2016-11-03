ec3089497804783d0213d046b325a5db7d460243
Joe Thorley <joe@poissonconsulting.ca>
2016-11-03 07:27:27 -0700

    Add ... to drop_parameters

--- R/drop-parameters.R
+++ R/drop-parameters.R
@@ -4,6 +4,7 @@
 #'
 #' @param x The object.
 #' @param parameters A character vector of the parameters to drop.
+#' @param ... Not used.
 #' @return The updated object.
 #' @export
-drop_parameters <- function(x, parameters = character(0)) {UseMethod("drop_parameters")}
+drop_parameters <- function(x, parameters = character(0), ...) {UseMethod("drop_parameters")}
