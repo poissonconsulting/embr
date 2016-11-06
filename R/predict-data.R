414e8c64c21238dde5bab8e9dfe21b426c46b29c
Joe Thorley <joe@poissonconsulting.ca>
2016-11-06 13:56:44 -0800

    Add modify_new_data arg to predict_data

--- R/predict-data.R
+++ R/predict-data.R
@@ -8,6 +8,7 @@
 #' @param term A string of the term in new_expr.
 #' @param conf_int A flag indicating whether to calculate confidence intervals.
 #' @param conf_level A number specifying the confidence level. By default 0.95.
+#' @param modify_new_data A single argument function to modify new data (in list form) immediately prior to calculating new_expr.
 #' @param quick A flag indicating whether to quickly get unreliable values.
 #' @param quiet A flag indicating whether to disable tracing information.
 #' @param beep A flag indicating whether to beep on completion of the analysis.
@@ -15,12 +16,14 @@
 #' @export
 predict_data <- function(data, analysis, new_expr = NULL, term = "prediction",
                          conf_int = FALSE, conf_level = 0.95,
+                         modify_new_data = NULL,
                          quick = getOption("mb.quick", FALSE),
                          quiet = getOption("mb.quiet", TRUE),
                          beep = getOption("mb.beep", FALSE),
                          ...) {
   check_mb_analysis(analysis)
   predict(analysis, new_data = data, new_expr = new_expr, term = term, conf_int = conf_int,
-          conf_level = conf_level, quick = quick, quiet = quiet, beep = beep, ...)
+          conf_level = conf_level, modify_new_data = modify_new_data,
+          quick = quick, quiet = quiet, beep = beep, ...)
 }
 
