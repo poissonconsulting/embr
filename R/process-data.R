28415d12c99030f260fb36a2b56ad409e405c02b
Joe Thorley <joe@poissonconsulting.ca>
2016-11-06 13:52:07 -0800

    Add process data

---
+++ R/process-data.R
@@ -0,0 +1,29 @@
+select_data <- function(data, select_data, center, scale, random_effects) {
+  if (!length(select_data)) {
+    cols <- c(center, scale)
+    cols %<>% c(unlist(random_effects))
+    check_cols(data, sort(unique(cols)))
+    return(data)
+  }
+
+  check_data2(data, select_data)
+  data <- data[names(select_data)]
+  data
+}
+
+#' Process Data
+#'
+#' Processes a data frame by selecting, centering and scaling data
+#'
+#' @param data The data to modify.
+#' @param data2 The base data.
+#' @param model An object inheriting from class tmb_model.#'
+#' @return The modified data in list form.
+#' @export
+process_data <- function(data, data2, model) {
+  check_mb_model(model)
+  data %<>% select_data(model$select_data, model$center, model$scale, model$random_effects)
+  data %<>% rescale::rescale(data2 = data2, center = model$center, scale = model$scale)
+  data %<>% as.list()
+  data
+}
