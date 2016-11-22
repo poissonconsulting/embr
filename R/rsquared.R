45a3426415132d4f0233b10dff813d981b113a72
Joe Thorley <joe@poissonconsulting.ca>
2016-11-22 11:12:43 -0800

    rsquared with response, prediction and discounted

--- R/rsquared.R
+++ R/rsquared.R
@@ -1,26 +1,57 @@
 #' R-Squared
 #'
-#' Calculate the proportion of the variance in the observed data explained
-#' by the fit.
+#' Calculate the proportion of the variance in the response predicted after discounting.
 #'
-#' @param analysis The analysis object.
-#' @param observed A string specifying the observed column in the data.
-#' @param ... Unused.
+#' @inheritParams predict_data
+#' @param response A string indicating the response term.
+#' @param prediction A string indicating the prediction term.
+#' @param discounted A string indicating the term to discount.
 #' @export
-rsquared <- function(analysis, observed, ...) {
+rsquared <- function(analysis, response = "response",
+                     prediction = "prediction", discounted = NULL,
+                     new_expr = NULL, modify_new_data = NULL,
+                     quick = getOption("mb.quick", FALSE),
+                     quiet = getOption("mb.quiet", TRUE),
+                     beep = getOption("mb.beep", FALSE), ...) {
   UseMethod("rsquared")
 }
 
 #' @export
-rsquared.mb_analysis <- function(analysis, observed, ...) {
-  check_string(observed)
-  data <- data_set(analysis)
-  if (!tibble::has_name(data, observed)) error("column '", observed, "' not found in data")
-  observed <- data[[observed]]
+rsquared.mb_analysis <- function(analysis, response = "response",
+                                 prediction = "prediction", discounted = NULL,
+                                 new_expr = NULL, modify_new_data = NULL,
+                                 quick = getOption("mb.quick", FALSE),
+                                 quiet = getOption("mb.quiet", TRUE),
+                                 beep = getOption("mb.beep", FALSE), ...) {
+  check_string(response)
+  check_string(fitted)
+  if (!is.null(discounted)) check_string(discounted)
 
-  fit <- fitted(analysis)$estimate
-  if (length(fit) != nrow(data)) error(observed, " and fitted values are not the same length")
+  check_flag(beep)
+  if (beep) on.exit(beepr::beep())
 
-  r2 <- 1 - var(observed - fit) / var(observed)
+
+  response <- predict(analysis, term = response, new_expr = new_expr,
+                      modify_new_data = modify_new_data, quick = quick, quiet = quiet,
+                      beep = FALSE)$estimate
+
+  prediction <- predict(analysis, term = prediction, new_expr = new_expr,
+                        modify_new_data = modify_new_data, quick = quick, quiet = quiet,
+                        beep = FALSE)$estimate
+
+  if (!identical(length(response), length(prediction))) error("response and prediction values must be the same length")
+
+  if (!is.null(discounted)) {
+    discounted <- predict(analysis, term = discounted, new_expr = new_expr,
+                          modify_new_data = modify_new_data, quick = quick, quiet = quiet,
+                          beep = FALSE)$estimate
+    if (!identical(length(response), length(discounted)))
+      error("response and discounted values must be the same length")
+
+    response <- response - discounted
+    prediction <- prediction - discounted
+  }
+
+  r2 <- 1 - var(response - prediction) / var(response)
   r2
 }
