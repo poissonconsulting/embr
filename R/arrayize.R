583386ec87a28023dbb686ebe5c840268a0b66e5
Joe Thorley <joe@poissonconsulting.ca>
2016-11-22 14:45:47 -0800

    arrayize on separate array

--- R/arrayize.R
+++ R/arrayize.R
@@ -1,35 +1,32 @@
 #' Arrayize
 #'
-#' Converts a vector in a named list into a matrix or array based on two or more factors.
+#' Converts a vector into a matrix or array based on two or more factors.
 #'
 #' The function can be used in modify_data functions to convert vectors to matrices or
 #' arrays.
 #'
-#' @param list The named list with the vector and factors.
-#' @param vector A string specifying the vector.
-#' @param factors A character vector specifying two or more factors
-#' @return The modified list.
+#' @param vector The vector.
+#' @param factors A named list of two or more factors.
+#' @return An array.
 #' @export
-arrayize <- function(list, vector, factors) {
-  check_uniquely_named_list(list)
-  check_string(vector)
-  check_vector(factors, "", min_length = 2)
-  check_unique(factors)
-  if (vector %in% factors) error("vector cannot be in factors")
-  if (!vector %in% names(list)) error("vector is missing from list")
-  if (!all(factors %in% names(list))) error("factors are missing from list")
-  if (!all(vapply(list[factors], is.factor, TRUE)))
-    error("factors in list must be factors")
+arrayize <- function(vector, factors) {
+  if (!is.vector(vector)) error("vector must be a vector")
+  if (is.list(vector)) error("vector must not be a list")
+  check_uniquely_named_list(factors)
+  if (length(factors) < 2) error("factors must be at least of length 2")
+  if ("vector" %in% names(factors)) error("factors must not be named 'vector'")
 
-  lengths <- vapply(list[c(vector, factors)], length, 1L)
+  lengths <- vapply(c("vector" = list(vector), factors), length, 1L)
   if (!all(lengths == lengths[1])) error("vector and factors must be the same lengths")
 
-  data <- as.data.frame(list[c(vector, factors)])
+  data <- as.data.frame(c("vector" = list(vector), factors))
   data %<>% unique()
 
-  dims <- vapply(list[factors], nlevels, 1L)
+  factors %<>% names()
+
+  dims <- vapply(data[factors], nlevels, 1L)
   names(dims) <- NULL
-  dimnames <- lapply(list[factors], levels)
+  dimnames <- lapply(data[factors], levels)
 
   if (nrow(data) != prod(dims))
     error("length of vector is not the product of the factor levels")
@@ -39,10 +36,8 @@ arrayize <- function(list, vector, factors) {
   }
 
   if (length(factors) == 2) {
-    list[[vector]] <- matrix(data[[vector]], nrow = dims[1],
-                             ncol = dims[2],
-                             dimnames = dimnames)
+    vector <- matrix(data$vector, nrow = dims[1], ncol = dims[2], dimnames = dimnames)
   } else
-    list[[vector]] <- array(data[[vector]], dim = dims, dimnames = dimnames)
-  list
+    vector <- array(data$vector, dim = dims, dimnames = dimnames)
+  vector
 }
