7870b99a02064b03af572153bbc9468375e5b2b9
Joe Thorley <joe@poissonconsulting.ca>
2016-11-25 10:59:40 -0800

    data_set can return modified data

--- R/data-set.R
+++ R/data-set.R
@@ -1,11 +1,24 @@
 #' Data Set
 #'
-#' Gets the data set for an object.
+#' Gets the data set for an object inheriting from class mb_analysis.
 #'
-#' @param x The object.
+#' @param analysis The object.
+#' @param modify A flag indicating whether to modify the data.
+#' @param ... Unused.
 #' @return The data set as a tibble.
 #' @export
-data_set <- function(x) {UseMethod("data_set")}
+data_set <- function(analysis, modify = FALSE, ...) {UseMethod("data_set")}
 
 #' @export
-data_set.mb_analysis <- function(x) x$data %>% dplyr::as.tbl()
+data_set.mb_analysis <- function(analysis, modify = FALSE, ...) {
+  check_flag(modify)
+  check_unused(...)
+
+  data <- analysis$data
+
+  if (modify) {
+    data %<>% modify_data(model(analysis))
+  } else
+      data %<>% dplyr::as.tbl()
+  data
+}
