042286c7315b6d1996f35bd592aaae6eee9a3670
Joe Thorley <joe@poissonconsulting.ca>
2016-12-14 14:41:41 -0800

    add AICw function

--- R/ic.R
+++ R/ic.R
@@ -8,3 +8,26 @@
 IC <- function(object, ...) {
   UseMethod("IC")
 }
+
+#' Akaike's Information Criterion Weight
+#'
+#' Calculates w_i for a list of objects of class mb_analysis that return AIC.
+#'
+#' @param x The object to calculate it for.
+#' @param ... Not used.
+#' @return A vector of the w_i values for each model.
+#' @export
+AICw <- function(x, ...) {
+  if (!is.list(x)) error("x must be a list")
+  if (!all(vapply(x, is.mb_analysis, TRUE))) error("x must be a list of mb_analysis objects")
+
+  data <- lapply(x, data_set)
+  if (!all(vapply(data, identical, TRUE, data[[1]]))) error("all elements of x must have the same data")
+
+  AIC <- vapply(x, AIC, 1)
+  AIC <- AIC - min(AIC)
+  AIC <- exp(-0.5 * AIC)
+  AIC <- AIC / sum(AIC)
+  AIC %<>% round(2)
+  AIC
+}
