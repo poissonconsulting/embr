2061e6356356c99bdb2417e7d1c2d7faef0e1430
joethorley <joe@poissonconsulting.ca>
2017-01-06 19:09:54 -0800

    fix is_converged

--- R/mcmcr.R
+++ R/mcmcr.R
@@ -34,3 +34,14 @@ nchains.mb_analysis <- function(x, ...) {
 niters.mb_analysis <- function(x, ...) {
   niters(as.mcmcr(x))
 }
+
+#' Is Converged
+#'
+#' @param x Object to test convergence for
+#' @param rhat A number specifying the rhat threshold.
+#' @param ... Unused
+#' @export
+is_converged.mb_analysis <- function(x, rhat = getOption("mb.rhat", 1.1), ...) {
+  check_number(rhat)
+  convergence(as.mcmcr(x)) <= rhat
+}
