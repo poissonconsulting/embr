8993a269f5c8a4db6c2a324270f3f072ab6cf06b
joethorley <joe@poissonconsulting.ca>
2017-01-09 16:55:27 -0800

    add parameters for mb_analysis

--- R/parameters.R
+++ R/parameters.R
@@ -8,3 +8,21 @@ parameters.character <- function(x, ...) {
 parameters.mb_code <- function(x, ...) {
   parameters(template(x))
 }
+
+#' @export
+parameters.mb_analysis <- function(x, param_type = "fixed", ...) {
+  check_scalar(param_type, c("fixed", "random", "derived"))
+
+  random <- names(random_effects(x))
+  if (is.null(random)) random <- character(0)
+  derived <- x$model$derived
+
+  if (identical(param_type, "fixed")) return(random)
+  if (identical(param_type, "derived")) return(derived)
+
+  parameters <- parameters(as.mcmcr(x))
+
+  parameters %<>% setdiff(random) %>% setdiff(derived)
+  parameters
+}
+
