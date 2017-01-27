e0a5a3d4de5b674beecc58927d01b017dab39351
joethorley <joe@poissonconsulting.ca>
2017-01-26 18:58:39 -0800

    dropped logLik and AICc from glance.mb_analysis

--- R/broom.R
+++ R/broom.R
@@ -3,8 +3,6 @@ glance.mb_analysis <- function(x, n = NULL, rhat = getOption("mb.rhat", 1.1), ..
   dplyr::data_frame(
     n = sample_size(x),
     K = nterms(x, include_constant = FALSE),
-    logLik = logLik(x),
-    AICc = AICc(x, n = n),
     minutes = elapsed(x),
     converged = converged(x, rhat = rhat)
   )
