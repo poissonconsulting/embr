decb0ffd40036b49da9d49ec2ed184db74d88cbb
joethorley <joe@poissonconsulting.ca>
2017-03-12 11:27:15 -0700

    remove plapply chuncks

--- R/utils.R
+++ R/utils.R
@@ -121,43 +121,6 @@ plapply <- function(X, FUN, .parallel = TRUE, ...) {
     FUN(X[[i]], ...)
 }
 
-#' Parallel lapply on chunks
-#'
-#' Applys function FUN to entire list X or possibly on chunks of X in parallel.
-#'
-#' @inheritParams base::lapply
-#' @param .parallel A flag indicating whether to analyse chunks in parallel.
-#' @export
-plapply_chunks <- function(X, FUN, .parallel = TRUE, ...) {
-    if (!is.list(X)) error("X must be a list")
-
-  if (!length(X)) return(X)
-
-  nworkers <- foreach::getDoParWorkers()
-
-  if (!.parallel || nworkers == 1 || length(X) == 1) {
-    return(FUN(X, ...))
-  }
-
-  names <- names(X)
-  indices <- parallel::splitIndices(length(X), nworkers)
-  print(X)
-  print(indices)
-  X <- plyr::llply(indices, function(i, x) x[i], x = X)
-  print("ou")
-  print(X)
-  X %<>% plapply(FUN = FUN, .parallel = TRUE, ...)
-  print("after")
-  print(X)
-  print(length(X))
-  print(lapply(X, length))
-  X %<>% unlist(recursive = FALSE)
-  print(X)
-  print(length(X))
-  names(X) <- names
-  X
-}
-
 #' Power
 #'
 #' R equivalent to the C++  function.
