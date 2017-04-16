4a9d369e111970e1837b87a2dd10d17607fe42fa
joethorley <joe@poissonconsulting.ca>
2017-04-16 11:45:47 -0700

    adjust priors

---
+++ R/adjust-priors.R
@@ -0,0 +1,44 @@
+#' Adjust Priors
+#'
+#' Adjusts standard deviation of simple normal priors in object by multiplier.
+#'
+#' A simple normal distribution has a mean of 0 and a numeric standard deviation
+#' (without truncation).
+#'
+#' @inheritParams stringr::str_replace
+#' @param object The object to adjust the priors for.
+#' @param multiplier A number indicating the adjustment to the standard deviation as a multiplier.
+#' @param ...  Additional arguments.
+#' @export
+adjust_priors <- function(object, multiplier = 2, ...) {
+  UseMethod("adjust_priors")
+}
+
+#' @export
+adjust_priors.character <- function(object, multiplier = 2, ...) {
+  object %<>% code(object)
+  object %<>% adjust_priors(multiplier = multiplier, ...)
+  template(object)
+}
+
+#' @export
+adjust_priors.list <- function(object, multiplier = 2, ...) {
+  if (!length(object)) return(object)
+
+  object %<>% purrr::map(adjust_priors, multiplier = multiplier, ...)
+
+  object
+}
+
+#' @export
+adjust_priors.mb_model <- function(object, multiplier = 2, ...) {
+  code <- code(object)
+  code %<>% adjust_priors(multiplier = multiplier)
+  object %<>% update_model(code = code)
+  object
+}
+
+#' @export
+adjust_priors.mb_code <- function(object, multiplier = 2, ...) {
+  error("adjust is not defined for objects of the virtual class 'mb_code'")
+}
