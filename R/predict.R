7f3e3f8fde2a86fe4c7a5e700e23e7ea68201f60
joethorley <joe@poissonconsulting.ca>
2017-04-26 09:42:04 -0700

    starting work on plot_residuals

--- R/predict.R
+++ R/predict.R
@@ -24,32 +24,6 @@ fitted.lmb_analysis <- function(object, ...) {
   predict(object, new_data = data_set(object))
 }
 
-#' Residuals
-#'
-#' Extract residual values for an MB analysis.
-#'
-#' The new_expr in the model must include the term 'residual'.
-#'
-#' @param object The MB analysis object.
-#' @param ... Unused.
-#' @return The analysis data set with the residual values.
-#' @export
-residuals.mb_analysis <- function(object, ...) {
-  predict(object, new_data = data_set(object), term = "residual")
-}
-
-#' Residuals
-#'
-#' Extract residual values for an LMB analysis.
-#'
-#' @param object The MB analysis object.
-#' @param ... Unused.
-#' @return The analysis data set with the residual values.
-#' @export
-residuals.lmb_analysis <- function(object, ...) {
-  .NotYetImplemented()
-}
-
 #' Predict
 #'
 #' @inheritParams derive_data
---
+++ R/residuals.R
@@ -0,0 +1,32 @@
+#' Residuals
+#'
+#' Extract residual values for an MB analysis.
+#'
+#' The new_expr in the model must include the term 'residual'.
+#'
+#' @param object The MB analysis object.
+#' @param ... Unused.
+#' @return The analysis data set with the residual values.
+#' @export
+residuals.mb_analysis <- function(object, ...) {
+  predict(object, new_data = data_set(object), term = "residual")
+}
+
+#' Residuals
+#'
+#' Extract residual values for an LMB analysis.
+#'
+#' @param object The MB analysis object.
+#' @param ... Unused.
+#' @return The analysis data set with the residual values.
+#' @export
+residuals.lmb_analysis <- function(object, ...) {
+  .NotYetImplemented()
+}
+
+plot_residuals <- function(x) {
+  if (!is.mb_analysis(x)) error("x must be an mb_analysis object")
+  residuals <- residuals(x)
+  .NotYetImplemented()
+}
+
