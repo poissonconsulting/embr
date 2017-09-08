9656bc2672a6ee9e021b4f24ebccb6c9e362f114
Joe Thorley <joe@poissonconsulting.ca>
2017-09-08 14:13:32 -0700

    data_set arg numericize_factors

--- R/data-set.R
+++ R/data-set.R
@@ -4,20 +4,21 @@
 #'
 #' @param analysis The object.
 #' @param modify A flag indicating whether to modify the data.
+#' @param numericize_factors A flag indicating whether to convert factors to integers if modifying the data.
 #' @param ... Unused.
 #' @return The data set as a tibble.
 #' @export
-data_set <- function(analysis, modify = FALSE, ...) {UseMethod("data_set")}
+data_set <- function(analysis, modify = FALSE, numericize_factors = FALSE, ...) {UseMethod("data_set")}
 
 #' @export
-data_set.mb_analysis <- function(analysis, modify = FALSE, ...) {
+data_set.mb_analysis <- function(analysis, modify = FALSE, numericize_factors = FALSE, ...) {
   check_flag(modify)
-  
+  check_flag(numericize_factors)
 
   data <- analysis$data
 
   if (modify) {
-    data %<>% modify_data(model(analysis))
+    data %<>% modify_data(model(analysis), numericize_factors = numericize_factors)
   } else
       data %<>% dplyr::as.tbl()
   data
