13cf9da77c21d74b789ad95a334b858599d5f491
Joe Thorley <joe@poissonconsulting.ca>
2017-10-04 12:02:28 -0700

    load_model.mb_analysis

--- R/load-model.R
+++ R/load-model.R
@@ -1,8 +1,15 @@
 #' Load Model
 #'
 #' @param x The model to load.
+#' @param quiet A flag indicating whether to suppress warnings and output.
 #' @param ...  Additional arguments.
 #' @export
-load_model <- function(x, ...) {
+load_model <- function(x, quiet, ...) {
   UseMethod("load_model")
+}
+
+
+#' @export
+load_model.mb_analysis <- function(x, quiet, ...) {
+  load_model(model(x), quiet, ...)
 }
