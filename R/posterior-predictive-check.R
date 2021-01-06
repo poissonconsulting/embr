9ca64ca39835a1cae9bb06ef032f08ea3871eaf4
Joe Thorley <joe@poissonconsulting.ca>
2021-01-06 13:26:58 -0800

    - Added `posterior_predictive_check()`.

---
+++ R/posterior-predictive-check.R
@@ -0,0 +1,53 @@
+#' Posterior Predictive Check
+#'
+#' Simulates
+#'
+#' @param x The object
+#' @param ... Unused.
+#' @return A tibble of the checks.
+#' @export
+posterior_predictive_check <- function(x, ...) {
+  UseMethod("posterior_predictive_check", x)
+}
+
+#' Posterior Predictive Check
+#'
+#' @param x The MB analysis object.
+#' @param ... Unused.
+#' @return A tibble of the checks.
+#' @export
+posterior_predictive_check.mb_analysis <- function(x, ...) {
+  chk_unused(...)
+  data <- data_set(x)
+
+  resample_residuals <- mcmc_derive_data(x, new_data = data, term = "^resample_residual$")
+  residuals <- predict(x, new_data = data, term = "residual")$estimate
+
+  resample_residuals <- as.mcmc(collapse_chains(resample_residuals$mcmc))
+
+  zeros <- extras::zeros(residuals, na_rm = TRUE)
+  mean <- extras::xtr_mean(residuals, na_rm = TRUE)
+  variance <- extras::variance(residuals, na_rm = TRUE)
+  skewness <- extras::skewness(residuals, na_rm = TRUE)
+  kurtosis <- extras::kurtosis(residuals, na_rm = TRUE)
+
+  zeroses <- apply(resample_residuals, 1, FUN = extras::zeros, na_rm = TRUE)
+  means <- apply(resample_residuals, 1, FUN = extras::xtr_mean, na_rm = TRUE)
+  variances <- apply(resample_residuals, 1, FUN = extras::variance, na_rm = TRUE)
+  skewnesses <- apply(resample_residuals, 1, FUN = extras::skewness, na_rm = TRUE)
+  kurtoses <- apply(resample_residuals, 1, FUN = extras::kurtosis, na_rm = TRUE)
+
+  tibble <- tibble::tibble(moment = c("zeros", "mean", "variance", "skewness", "kurtosis"))
+  tibble$moment <- factor(tibble$moment, tibble$moment)
+  tibble$observed <- c(zeros, mean, variance, skewness, kurtosis)
+  tibble$median <- c(median(zeros), median(means), median(variances), median(skewnesses), median(kurtoses))
+  tibble$lower <- c(lower(zeros), lower(means), lower(variances), lower(skewnesses), lower(kurtoses))
+  tibble$upper <- c(upper(zeros), upper(means), upper(variances), upper(skewnesses), upper(kurtoses))
+  tibble$svalue <- c(svalue(zeroses, threshold = zeros),
+                     svalue(means, threshold = mean),
+                     svalue(variances, threshold = variance),
+                     svalue(skewnesses, threshold = skewness),
+                     svalue(kurtoses, threshold = kurtosis))
+
+  tibble
+}
