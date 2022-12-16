8e8bab710676994a858bef543353d5b51046bc01
Joe Thorley <joe@poissonconsulting.ca>
2022-12-16 12:18:23 -0800

    expect warning

--- tests/testthat/test-pars.R
+++ tests/testthat/test-pars.R
@@ -1,14 +1,14 @@
 test_that("pars", {
 
-  new_expr = mb_code(" fit2 <- a + b * x # c
+  expect_warning(new_expr <- mb_code(" fit2 <- a + b * x # c
                     fit <- a + b * x + bYear[Year]
                      residual <- y - fit
                     for(i in 1:2) {
                      prediction[i] <- fit[i]
-                    }")
+                    }"), "^template type is unrecognised")
 
   expect_identical(pars(new_expr), sort(c("a", "b", "bYear", "fit", "fit2", "i", "prediction",
-                         "residual", "x", "y", "Year")))
+                                          "residual", "x", "y", "Year")))
 
   expect_identical(npars(new_expr), 11L)
 })
