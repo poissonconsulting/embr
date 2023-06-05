4050e458bb62fd108f0d25414e38613ae7e9bb0c
Kirill Müller <krlmlr+r@mailbox.org>
2023-06-05 19:11:33 +0200

    Document

--- R/embr-package.R
+++ R/embr-package.R
@@ -1,7 +1,15 @@
 #' @keywords internal
 "_PACKAGE"
 
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom lifecycle deprecate_soft
+#' @importFrom rlang enquo
 #' @importFrom rlang eval_tidy
+#' @importFrom rlang inject
+#' @importFrom rlang list2
+#' @importFrom rlang quo_get_expr
+#' @importFrom rlang quo_is_null
 ## usethis namespace: end
 NULL
