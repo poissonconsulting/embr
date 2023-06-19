9febc4e21c1a8813bbb7b2399e35864ca2999d43
Ayla Pearson <ayla@poissonconsulting.ca>
2023-06-19 14:46:51 -0700

    update docs

--- R/new-expr.R
+++ R/new-expr.R
@@ -78,8 +78,8 @@ new_expr.mb_meta_analyses <- function(object, ...) {
 #'
 #' @param new_expr Must be passed as `{{ new_expr }}` by the caller.
 #' @param default A quoted expression to use as fallback.
-#' @param vectorize Set to `TRUE` to call `mcmcderive::expression_vectorize()` on the result.
-#'   The current default is to not vectorize, this can change later.
+#' @param vectorize The current default is to vectorize. Set to `FALSE` to not
+#'  vectorize.
 #' @return `new_expr` quoted and (if needed) parsed, or `default` if `new_expr` is `NULL`.
 #' @noRd
 enexpr_new_expr <- function(new_expr, default = NULL, vectorize = NULL) {
