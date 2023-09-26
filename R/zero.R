d50a6b97a3201b29f9f2aace49201d6ccb37a57d
Kirill Müller <kirill@cynkra.com>
2023-09-26 16:47:17 -0700

    Only apply as.numeric() after selecting the relevant columns

--- R/zero.R
+++ R/zero.R
@@ -2,8 +2,8 @@ zero_random_effects <- function(mcmcr, data, random_effects) {
   stopifnot(all(names(random_effects) %in% pars(mcmcr)))
   stopifnot(all(unlist(random_effects) %in% names(data)))
 
-  data <- llply(data, as.numeric)
   data <- data[unique(unlist(random_effects))]
+  data <- lapply(data, as.numeric)
   data <- data[vapply(data, all1, TRUE)]
   data <- names(data)
 
