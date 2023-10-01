93686a449b1f7671748b9f4eeadb35bd41855568
Kirill Müller <kirill@cynkra.com>
2023-10-01 14:16:19 -0400

    Use rjags as engine name

--- R/pmb-analyse.R
+++ R/pmb-analyse.R
@@ -6,11 +6,11 @@ analyse.pmb_model <- function(
     engine = NULL) {
 
   # FIXME: Generic translation
-  if (identical(engine, "jmbr")) {
+  if (identical(engine, "rjags")) {
     x$code <- translate_jmbr(x$code)
     x <- new_mb_model(x, class(x$code))
   } else {
-    err('Must specify `engine = "jmbr"` for analysing engine-agnostic code')
+    err('Must specify `engine = "rjags"` for analysing engine-agnostic code')
   }
 
   analyse(x, data, ...)
