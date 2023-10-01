ea4fe49b77e75a79918318259dd01a8fe9d19442
Kirill Müller <kirill@cynkra.com>
2023-10-01 06:32:01 -0400

    Import all of rlang and lifecycle

--- R/embr-package.R
+++ R/embr-package.R
@@ -3,14 +3,8 @@
 
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
+#' @import rlang lifecycle
 ## usethis namespace: start
-#' @importFrom lifecycle deprecate_soft
-#' @importFrom rlang enquo
-#' @importFrom rlang eval_tidy
-#' @importFrom rlang inject
-#' @importFrom rlang list2
-#' @importFrom rlang quo_get_expr
-#' @importFrom rlang quo_is_null
 #' @importFrom utils packageVersion
 ## usethis namespace: end
 NULL
