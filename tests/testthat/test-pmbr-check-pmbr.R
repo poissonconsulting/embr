6d400906ac869e0d67828de19f18e7b3916c7040
Nicole Hill <nicole@poissonconsulting.ca>
2024-06-17 08:29:27 -0700

    non-change commit to restart actions

--- tests/testthat/test-pmbr-check-pmbr.R
+++ tests/testthat/test-pmbr-check-pmbr.R
@@ -15,7 +15,6 @@ test_that("check_pmbr()", {
 
 cli::test_that_cli("check_pmbr() errors", {
     local_edition(3)
-
     expect_snapshot(error = TRUE, {
       check_pmbr(expr({
         bY ~ dnorm(0, 2^-2)
